78a7269be55caec19b1c7de68e04425b62897339
Lidia Ali <laliquilla25@gmail.com>
2016-08-26 14:49:49 -0400

    images

--- client/app/datosAbiertos/datosAbiertos.controller.js
+++ client/app/datosAbiertos/datosAbiertos.controller.js
@@ -8,25 +8,21 @@ angular.module('cdmxIndicatorsApp')
 
     $scope.myModalContent = {
       indicator1: {
-        title: 'Gasto de capital',
-        description: 'Evolución del gasto de capital. Presenta el presupuesto original (o presupuesto aprobado) anual y el presupuesto modificado y ejercido (para el último mes disponible) del gasto de capital. También presenta el porcentaje de ejecución del gasto de capital (el cual se calcula dividiendo el presupuesto ejercido sobre el presupuesto modificado).'
+        title:'Evolución de la demanda de datos abiertos de la CDMX',
+        description: 'Número de descargas de datasets desde el portal de datos abiertos de la CDMX'
       },
       indicator2: {
-        title:'Gasto corriente',
-        description:'Evolución del gasto corriente. Presenta el presupuesto original (o presupuesto aprobado) anual y el presupuesto modificado y ejercido (para el último mes disponible) del gasto corriente. También presenta el porcentaje de ejecución del gasto corriente (el cual se calcula dividiendo el presupuesto ejercido sobre el presupuesto modificado).'
+        title:'Evolución de la oferta de datos abiertos de la CDMX',
+        description:'Número de datasets publicados en gobiernogobiernoabierto.cdmx.gob.mx'
       },
       indicator3: {
-        title:'Tres primeros en ejecución de gasto de capital',
-        description:'Presenta las tres Dependencias y las tres Actividades Institucionales con mayor ejecución de gasto de capital. Hacer clic en el botón de Dependencia o Actividad Institucional según lo que se quiera revisar.'
+        title:'Entidades con mayor oferta de datos abiertos',
+        description:'Número de datasets publicados en gobiernoabierto.cdmx.gob.mxpor entidad'
       },
       indicator4:{
-        title:'Ejecución del gasto por Dependencia',
-        description:'Presenta la ejecución del gasto corriente y la ejecución del gasto de capital para las cinco principales Dependencias según el presupuesto original (o presupuesto aprobado) anual. Si desea revisar la ejecución del gasto de otras Dependencias, debe seleccionar los nombres en el menú desplegable.'
+        title:'Entidades con mayor demanda de datos abiertos',
+        description:'Número de descargas de datos abiertos de la CDMX por entidad'
       },
-      indicator5:{
-        title:'Ejecución del gasto por función',
-        description:'Presenta la ejecución del gasto por función, de conformidad con la finalidad a la cual corresponde el gasto (gobierno, desarrollo social o desarrollo económico).'
-      }
     };
 
     //Codigo modal
