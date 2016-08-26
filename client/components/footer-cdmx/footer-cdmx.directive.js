053bab0dc92a0c89f6c1c4c8ce45a13e72921a1e
Lidia Ali <laliquilla25@gmail.com>
2016-08-26 15:54:40 -0400

    images and footer

--- client/components/footer-cdmx/footer-cdmx.directive.js
+++ client/components/footer-cdmx/footer-cdmx.directive.js
@@ -1,11 +1,26 @@
 'use strict';
 
 angular.module('cdmxIndicatorsApp')
-  .directive('footerCdmx', function () {
+  .directive('footerCdmx', function ($window) {
     return {
       templateUrl: 'components/footer-cdmx/footer-cdmx.html',
       restrict: 'EA',
       link: function (scope, element, attrs) {
+        console.log(element);
+        var fixedButton = element.find('.scrollTop-button');
+        angular.element($window).bind("scroll", function() {
+          if (angular.element($window).scrollTop() >= 200) {
+            fixedButton.show(10).animate({right: '15px'}, 10);
+          } else {
+            fixedButton.show(10).animate({right: '-80px'}, 10);
+          }
+        });
+        fixedButton.click(function (e) {
+            e.preventDefault();
+            angular.element('html,body').animate({
+                scrollTop: 0
+            }, 600);
+        });
       }
     };
   });
